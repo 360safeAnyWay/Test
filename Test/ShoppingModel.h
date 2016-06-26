7168db01d71cd579a4d02c4afa06193966d7697d
steven.ywj <steven.ywj@alibaba-inc.com>
2016-06-27 00:02:19 +0800

    add model data

---
+++ Test/ShoppingModel.h
@@ -0,0 +1,18 @@
+//
+//  ShoppingModel.h
+//  Test
+//
+//  Created by ywj on 16/6/26.
+//  Copyright © 2016年 yangwenjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ShoppingModel : NSObject
+
+@property (nonatomic, copy) NSString *shoppingID;
+@property (nonatomic, copy) NSString *desc;
+@property (nonatomic, assign) CGFloat price;
+
+@end
---
+++ Test/ShoppingModel.m
@@ -0,0 +1,13 @@
+//
+//  ShoppingModel.m
+//  Test
+//
+//  Created by ywj on 16/6/26.
+//  Copyright © 2016年 yangwenjun. All rights reserved.
+//
+
+#import "ShoppingModel.h"
+
+@implementation ShoppingModel
+
+@end
